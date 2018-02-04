0bf2ac92f777b57a3bb404b7dc593d5b107eaedf
alexmorten <alexandrius.martinus@gmx.de>
2018-02-04 16:10:04 +0100

    adjust to new dataset

--- producer.go
+++ producer.go
@@ -10,9 +10,10 @@ import (
 
 func main() {
 	config := sarama.NewConfig()
-	config.Producer.Return.Successes = true
+	config.Producer.Return.Successes = false
+	config.Producer.Return.Errors = false
 	brokerList := []string{"localhost:9092"}
-	producer, err := sarama.NewSyncProducer(brokerList, config)
+	producer, err := sarama.NewAsyncProducer(brokerList, config)
 	if err != nil {
 		panic(err)
 	}
@@ -22,12 +23,34 @@ func main() {
 		Topic:        "users",
 	}
 	http.Handle("/users", userHandler)
-	connectionHandler := handler.RequestHandler{
+
+	itemHandler := handler.RequestHandler{
 		Producer:     producer,
-		ProtoMessage: &protocol.Connection{},
-		Topic:        "connections",
+		ProtoMessage: &protocol.Item{},
+		Topic:        "items",
 	}
-	http.Handle("/connections", connectionHandler)
+	http.Handle("/items", itemHandler)
+
+	interactionHandler := handler.RequestHandler{
+		Producer:     producer,
+		ProtoMessage: &protocol.Interaction{},
+		Topic:        "interactions",
+	}
+	http.Handle("/interactions", interactionHandler)
+
+	targetItemHandler := handler.RequestHandler{
+		Producer:     producer,
+		ProtoMessage: &protocol.TargetItem{},
+		Topic:        "target_items",
+	}
+	http.Handle("/target_items", targetItemHandler)
+
+	targetUserHandler := handler.RequestHandler{
+		Producer:     producer,
+		ProtoMessage: &protocol.Interaction{},
+		Topic:        "target_users",
+	}
+	http.Handle("/target_users", targetUserHandler)
 
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		panic(err)
